fc4aca561f535b0928fa6898307c23dca000fae9
pablo <taddeiApablo23@gmail.com>
2024-09-12 00:35:06 -0300

    continuando con el curso

--- flutter1/test01/lib/screens/homescreen.dart
+++ flutter1/test01/lib/screens/homescreen.dart
@@ -1,10 +1,9 @@
-// aqui importamos la libreria necesaria para construir la GUI
+/// aqui importamos la libreria necesaria para construir la GUI
 import 'package:flutter/material.dart';
 
-/**
- * HomeScreen - aqui separamos la construccion de la GUI
- * la cual llamamos widget a un archivo por separado.
- */
+///HomeScreen - aqui separamos la construccion de la GUI
+///la cual llamamos widget a un archivo por separado.
+
 // construir la clase HomeScreen que extiende de StatelessWidget
 class HomeScreen extends StatelessWidget {
   const HomeScreen({
@@ -19,7 +18,38 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.green,
         title: const Text('Flutter 1'),
       ),
-      body: Container(),
+      body: Container(
+        margin: const EdgeInsets.only(top: 10),
+        width: double.infinity,
+        height: 350,
+        decoration: const BoxDecoration(color: Colors.white),
+        padding: const EdgeInsets.all(10),
+        child: Container(
+          child: const Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    child: Text(
+                      "P",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    backgroundColor: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("Pablo Taddei",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.black26))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
